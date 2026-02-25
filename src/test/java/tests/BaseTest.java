f32e6f874d343cf4bdd00d9372549bc7132e621b
viktorkhrystenko1 <viktor.khrystenko1@gmail.com>
2026-02-25 14:17:08 +0200

    [#1] Registration and login tests - added allure reports; - implemented test listener, that will make screenshots on each failed test.

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,15 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Listeners;
+import testnglisteners.ScreenshotListener;
+
+@Listeners(ScreenshotListener.class)
 public class BaseTest {
+    private WebDriver driver;
+
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
